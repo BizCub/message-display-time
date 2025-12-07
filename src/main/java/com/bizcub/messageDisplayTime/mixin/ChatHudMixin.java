30074e4cc2740392252c64d24592641df3a40666
BizCub <bizcubmine@gmail.com>
2025-12-07 18:10:06 +0200

    The mod's mappings for older versions of the game have been changed to MojMaps

--- src/main/java/com/bizcub/messageDisplayTime/mixin/ChatHudMixin.java
+++ src/main/java/com/bizcub/messageDisplayTime/mixin/ChatHudMixin.java
@@ -7,28 +7,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-//? >=1.21.11 {
 import net.minecraft.client.gui.components.ChatComponent;
 
 @Mixin(ChatComponent.class)
 public class ChatHudMixin {
 
-    @ModifyVariable(method = "render", at = @At(value = "HEAD"), argsOnly = true, ordinal = 0)
+    @ModifyVariable(method = "render*", at = @At(value = "HEAD"), argsOnly = true, ordinal = 0)
     private int render(int currentTick) {
         if (Compat.isModLoaded(MessageDisplayTime.clothConfigId)) return currentTick + 200 - (20 * Configs.getInstance().sec);
         return currentTick + 100;
     }
 }
-
-//?} <=1.21.10 {
-/*import net.minecraft.client.gui.hud.ChatHud;
-
-@Mixin(ChatHud.class)
-public class ChatHudMixin {
-
-    @ModifyVariable(method = "render", at = @At(value = "HEAD"), argsOnly = true, ordinal = 0)
-    private int render(int currentTick) {
-        if (Compat.isModLoaded(MessageDisplayTime.clothConfigId)) return currentTick + 200 - (20 * Configs.getInstance().sec);
-        return currentTick + 100;
-    }
-}*///?}

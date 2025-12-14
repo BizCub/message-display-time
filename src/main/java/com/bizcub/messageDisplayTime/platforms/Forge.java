6d0043f127d075df3b0907a011681f6ae5badec1
BizCub <bizcubmine@gmail.com>
2025-12-14 17:53:12 +0200

    Forge config screen fix

--- src/main/java/com/bizcub/messageDisplayTime/platforms/Forge.java
+++ src/main/java/com/bizcub/messageDisplayTime/platforms/Forge.java
@@ -4,10 +4,10 @@
 import com.bizcub.messageDisplayTime.MessageDisplayTime;
 import com.bizcub.messageDisplayTime.config.Configs;
 import me.shedaniel.autoconfig.AutoConfig;
-/^? >=1.19^/ /^import net.minecraftforge.client.ConfigScreenHandler;^/
+/^? >=1.19^/ import net.minecraftforge.client.ConfigScreenHandler;
 /^? >=1.18 && <=1.18.2^/ /^import net.minecraftforge.client.ConfigGuiHandler;^/
 /^? >=1.17 && <=1.17.1^/ /^import net.minecraftforge.fmlclient.ConfigGuiHandler;^/
-/^? <=1.16.5^/ import net.minecraftforge.fml.ExtensionPoint;
+/^? <=1.16.5^/ /^import net.minecraftforge.fml.ExtensionPoint;^/
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
@@ -17,23 +17,23 @@ public class Forge {
     public Forge() {
         MessageDisplayTime.init();
 
-        //? >=1.19 {
+        //? >=1.19 && <=1.21.3 {
         /^ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> {
             return new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> {
                 return PlatformInit.getScreen(screen);
             });
         });
 
-        ^///?} >=1.17 {
+        ^///?} >=1.17 && <=1.18.2 {
         /^ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class,
                 () -> new ConfigGuiHandler.ConfigGuiFactory((client, parent) ->
                         PlatformInit.getScreen(parent))
         );
 
-        ^///?} else {
-        ModLoadingContext.get().registerExtensionPoint(
+        ^///?} <=1.16.5 {
+        /^ModLoadingContext.get().registerExtensionPoint(
                 ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, screen) ->
                         PlatformInit.getScreen(screen)
-        );//?}
+        );^///?}
     }
 }*///?}
